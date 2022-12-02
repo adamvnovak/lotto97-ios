0ed3b01535aba3a7e06ef5e89bac0a1ec19ea542
adamvnovak <adamvnovak@protonmail.com>
2022-12-02 15:23:06 -0800

    Initial Commit

---
+++ lotto97/lotto97App.swift
@@ -0,0 +1,17 @@
+//
+//  lotto97App.swift
+//  lotto97
+//
+//  Created by Adam Novak on 2022/12/02.
+//
+
+import SwiftUI
+
+@main
+struct lotto97App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
